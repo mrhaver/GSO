5af4bede4660a5a2b2ac92b9cb17cd183639b2f4
alexras95 <alexras@live.nl>
2015-10-14 12:37:45 +0200

    RMIServer klasse done-zo

--- AEXBanner_Week7/src/aexbanner_week7/RMIServer.java
+++ AEXBanner_Week7/src/aexbanner_week7/RMIServer.java
@@ -5,6 +5,7 @@
  */
 package aexbanner_week7;
 
+import java.net.InetAddress;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -42,10 +43,15 @@ public class RMIServer {
         }
     }
     
-    public static void main(String[] args) {
+    private static void printIP() throws java.net.UnknownHostException{
+        InetAddress localhost = InetAddress.getLocalHost();
+        System.out.println("Server: IP Address: " + localhost.getHostAddress());
+    }
+    
+    public static void main(String[] args) throws java.net.UnknownHostException {
 
         System.out.println("SERVER USING REGISTRY");
-
+        printIP();
         RMIServer server = new RMIServer();
     }
 }
