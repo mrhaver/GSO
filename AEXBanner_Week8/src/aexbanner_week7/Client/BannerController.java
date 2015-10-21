7762e3fc4521b2c737a4d23792bca9c9576b97b6
mrhaver <f.haver@student.fontys.nl>
2015-10-21 12:34:49 +0200

    update

--- AEXBanner_Week8/src/aexbanner_week7/Client/BannerController.java
+++ AEXBanner_Week8/src/aexbanner_week7/Client/BannerController.java
@@ -8,6 +8,8 @@ package aexbanner_week7.Client;
 import aexbanner_week7.Client.AEXBanner;
 import aexbanner_week7.Shared.IEffectenbeurs;
 import aexbanner_week7.Server.IFonds;
+import aexbanner_week7.Shared.RemotePropertyListener;
+import java.beans.PropertyChangeEvent;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -20,28 +22,56 @@ import java.util.TimerTask;
 /**
  * @Author Frank Haver
  */
-public class BannerController {
+public class BannerController implements RemotePropertyListener{
 
     private AEXBanner banner;    
-    private Timer pollingTimer;
-    private Timer fluctuatieTimer;
     private ArrayList<IFonds> huidigeKoersen;
     
     private static final String bindingName = "Effectenbeurs";
     private Registry registry = null;
     private IEffectenbeurs effectenbeurs = null;
+    private String ipAddress;
+    private int portNumber;
 
     public BannerController(AEXBanner banner, String ipAddress, int portNumber) {
 
         this.banner = banner;
-        
-        // Start polling timer: update banner every two seconds
-        pollingTimer = new Timer();
-        
-        fluctuatieTimer = new Timer();
-        
-        // TODO 
-        
+        this.ipAddress = ipAddress;
+        this.portNumber = portNumber;
+    }
+    
+    // Print contents of registry
+    private void printContentsRegistry() {
+        try {
+            String[] listOfKoers = registry.list();
+            System.out.println("Client: list of koersen bound in registry:");
+            if (listOfKoers.length != 0) {
+                for (String s : registry.list()) {
+                    System.out.println(s);
+                }
+            } else {
+                System.out.println("Client: list of koersen bound in registry is empty");
+            }
+        } catch (RemoteException ex) {
+            System.out.println("Client: Cannot show list of koersen bound in registry");
+            System.out.println("Client: RemoteException: " + ex.getMessage());
+        }
+    }
+    
+    public String getHuidigeKoersString(){
+        String koersen = "";
+        for(IFonds i : huidigeKoersen){
+             koersen = koersen + i.getNaam() + ": " + String.valueOf(i.getKoers()) + " ";
+        }                
+        return koersen;
+    }
+
+    // Stop banner controller
+    public void stop() {
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) throws RemoteException {
         // Print IP address and port number for registry
         System.out.println("Client: IP Address: " + ipAddress);
         System.out.println("Client: Port number " + portNumber);
@@ -93,77 +123,12 @@ public class BannerController {
         
         try{
             huidigeKoersen = effectenbeurs.getKoersen();
-            fluctuatieTimer.schedule(new FluctuatieTask(), 0,2000);
         }
         catch(RemoteException ex){
             System.out.println(ex.getMessage());
         }
         
-        
-        
-    }
-    
-    // Print contents of registry
-    private void printContentsRegistry() {
-        try {
-            String[] listOfKoers = registry.list();
-            System.out.println("Client: list of koersen bound in registry:");
-            if (listOfKoers.length != 0) {
-                for (String s : registry.list()) {
-                    System.out.println(s);
-                }
-            } else {
-                System.out.println("Client: list of koersen bound in registry is empty");
-            }
-        } catch (RemoteException ex) {
-            System.out.println("Client: Cannot show list of koersen bound in registry");
-            System.out.println("Client: RemoteException: " + ex.getMessage());
-        }
-    }
-    
-    class FluctuatieTask extends TimerTask{
-
-        @Override
-        public void run() {
-            // fluctueer de huidige koersen
-            // verander met minimaal 0 en maximaal 5% van de koers
-            // kan negatief of positief zijn.
-            Random rnd = new Random(); 
-            for(IFonds ifo : huidigeKoersen){
-                double rangeMin = 0;
-                double rangeMax = ifo.getKoers()/20d;
-                double randomDouble = rangeMin + (rangeMax - rangeMin) * rnd.nextDouble();
-                int randomInt = 0 + (int)(Math.random() * ((1 - 0) + 1));
-                randomDouble = (double)Math.round(randomDouble * 100) / 100d;
-                double nieuwWaarde;
-                if(randomInt == 0){
-                    nieuwWaarde = (double)Math.round((ifo.getKoers() - randomDouble) *100) / 100d;
-                }
-                else{
-                    nieuwWaarde = (double)Math.round((ifo.getKoers() + randomDouble) *100) / 100d;
-                }
-                ifo.setKoers(nieuwWaarde);                
-            }
-            
-            banner.setKoersen(getHuidigeKoersString());
-        }
-        
-    }
-    
-    public String getHuidigeKoersString(){
-        String koersen = "";
-        for(IFonds i : huidigeKoersen){
-             koersen = koersen + i.getNaam() + ": " + String.valueOf(i.getKoers()) + " ";
-        }                
-        return koersen;
-    }
-
-    // Stop banner controller
-    public void stop() {
-        pollingTimer.cancel();
-        // Stop simulation timer of effectenbeurs
-        // TODO
-        fluctuatieTimer.cancel();
+        banner.setKoersen(getHuidigeKoersString());
     }
 }
 
